71d19dc25b7c23b61ec629fe924779fda3633c4e
VengDevs <fayngame@gmail.com>
2024-03-01 18:13:12 +0300

    changed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
   name='asyncj',
-  version='0.1.0',
+  version='1.1.0',
   author='VengDevs',
   description='Python module for fast asynchronous work with JSON files',
   long_description=readme(),
